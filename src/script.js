c7a7d3f410ca7bb79ddcc2c1b6b7f64f0e4f7d95
bruno <mguimaraesbruno@gmail.com>
2018-08-07 23:19:59 -0300

    New

---
+++ src/script.js
@@ -0,0 +1,15 @@
+//My first Blackjack Game
+//Bruno Moraes Guimarães
+
+let deck = [
+        "Ace of Spades",
+        "Two of Spades",
+        "Three of Spades",
+];
+
+let playerCards = [ deck[0], deck[2] ];
+console.log("Blackjack, the game!");
+
+console.log("You are dealt: ");
+console.log("  " + playerCards[0]);
+console.log("  " + playerCards[2]);
